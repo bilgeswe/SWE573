b9cfa5c2e9f959d51197224d37d48e8dbb35a1a1
Bilge Akpulat <bilgetheprofessional@bilges-air.station>
2024-10-22 17:14:43 +0300

    The first commit where django is created

---
+++ forum/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user
+            return redirect('home')  # Redirect to the home page or any other page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
