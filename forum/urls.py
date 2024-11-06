cb9b8cc0e908ecc7a093dde16cd243a91ac3a371
Bilge Akpulat <bilgetheprofessional@Bilges-MacBook-Air.local>
2024-11-06 10:31:10 +0300

    The code had many errors, hard to detect and decided to move on from main branch

--- forum/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path                                 # Function to define URL patterns
-from .views import register, user_login, home, create_object_post  # Import the view functions for registration, login, and creating a post
-                                                            # Define URL patterns for the application
-urlpatterns = [
-    path('', home, name='home'),                            # The default home page
-    path('register/', register, name='register'),           # URL pattern for the registration view
-    path('login/', user_login, name='login'),               # URL pattern for the login view
-    path('create-post/', create_object_post, name='create_post'),  # URL pattern for the create post view
-]
--- forum_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin            # Import the admin module to access Django's built-in admin site
-from django.urls import path, include       # Import functions to define URL patterns and include other URL configurations
-from django.shortcuts import redirect       # Import redirect to use for the root URL
-
-urlpatterns = [                             # Define the URL patterns for the project
-    path('admin/', admin.site.urls),        # URL pattern for the Django admin site, accessible at /admin/
-    path('', lambda request: redirect('register/')),  # Redirect root URL to 'register' page
-    path('', include('forum.urls')),        # Route to include URLs from 'forum' app
-]
