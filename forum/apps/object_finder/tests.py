cd28295fed7d660d3a09f1cce9bbbb3818aca042
bilge.swe <bilge.swe@gmail.com>
2024-12-17 00:41:38 +0300

    Wrote unit tests to see how the functionalities behave in the app.

--- forum/apps/object_finder/tests.py
+++ forum/apps/object_finder/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 from .models import Post
 
 class UserTestCase(TestCase):
@@ -23,4 +24,73 @@ class UserTestCase(TestCase):
         saved_user = User.objects.get(username='testuser')
         self.assertEqual(saved_user.username, test_user.username)
         self.assertEqual(saved_user.email, test_user.email)
-        
+
+        def test_post_creation_count(self):
+            """Test that creating a post via form URL increases post count"""
+            # Get initial post count
+            initial_count = Post.objects.count()
+                
+            # Set up test client and user
+            client = Client()
+            user = User.objects.create_user(
+                username='postuser',
+                email='post@test.com', 
+                password='testPass123..'
+            )
+            client.login(username='postuser', password='testPass123..')
+            # Make POST request to form URL to create post
+            post_data = {
+                'title': 'Test Post',
+                'content_delta': '{}',
+                'header_image': SimpleUploadedFile(
+                    name='test_image.jpg',
+                    content=b'',
+                    content_type='image/jpeg'
+                )
+            }
+            response = client.post('/form', post_data)
+                
+            # Verify post count increased by 1
+            self.assertEqual(Post.objects.count(), initial_count + 1)
+                
+            # Verify post was created with correct data
+            latest_post = Post.objects.latest('date_posted')
+            self.assertEqual(latest_post.title, 'Test Post')
+            self.assertEqual(latest_post.author, user)
+
+            def test_comment_creation(self):
+                """Test that creating a comment on a post works"""
+                # Create test user and post
+                user = User.objects.signup(
+                    username='commentuser',
+                    password='testPass123..'
+                )
+                
+                post = Post.objects.create(
+                    title='Test Post',
+                    content_delta='{}',
+                    author=user
+                )
+
+                # Create a comment on the post
+                comment = Comment.objects.crate(
+                    content='Test comment',
+                    post=post,
+                    author=user
+                )
+
+                # Verify comment was created and associated with post
+                self.assertEqual(Comment.objects.count(), 1)
+                self.assertEqual(post.comments.count(), 1)
+                self.assertEqual(comment.content, 'Test comment')
+                self.assertEqual(comment.author, user)
+                self.assertEqual(comment.post, post)
+
+    def test_index_page_accessible(self):
+        """Test that the index page is accessible to all users"""
+        # Create test client
+        client = Client()
+        # Make GET request to index page
+        response = client.get('/')
+        # Verify successful response
+        self.assertEqual(response.status_code, 200)
